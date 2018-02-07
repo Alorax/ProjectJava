fde10412578125bbc3988773daa3a36d8e8d6945
Alorax <tropa1996@gmail.com>
2018-02-07 23:31:07 +0000

    Added comment to Main.java.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,17 @@
+// Main.java
+// Authors: Miruna Serian and Anima Sutradhar.
+// Program last updated: 7th February 2018.
+
+/* This program helps teams allocate the credit for a project fairly so that all
+   parties are satisfied with the outcome.                                          */
+
+
+/*
+------------------------------------------------------------------------------------
+    This is the Main class where the project is run.
+------------------------------------------------------------------------------------
+*/
+
 public class Main
 {
     public static void main(String[] args)
