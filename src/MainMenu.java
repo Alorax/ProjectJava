3385219c59d1ed7a63f4391ccfad39b7d2a0981d
Alorax <tropa1996@gmail.com>
2018-02-07 13:39:26 +0000

    Working project.

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,84 +1,91 @@
-// This program is a Fair Grade Allocator, with the purpose of helping
-// teams allocate the credit for a project fairly so that all parties
-// are satisfied with the outcome.
-
-// This is the first deliverable of the Fair Grade Allocator that shows
-// the user the Main Menu, and allows them to create a project.
-
-// Author: Anima Sutradhar.
-// Last update: Sunday 4th January 2018.
-
-//------------------------------------------------------------------------
-// The Main Menu page that will be the first thing the user sees.
-//------------------------------------------------------------------------
-
 import java.util.Scanner;
-import static java.lang.String.*;
 
-public class MainMenu
-{
-    //Creating the Main Menu options
 
-    public static void main(String[] args)
-    {
-        Scanner scan = new Scanner(System.in);
+class Project {
+    String name;
+    String[] namesOfTeamMembers;
 
-        //Main Menu content
-        System.out.println("Welcome to Split-it");
-        System.out.println("\n\tAbout (A)");
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getNamesOfTeamMembers() {
+        return namesOfTeamMembers;
+    }
+
+    public void setNamesOfTeamMembers(String[] namesOfTeamMembers) {
+        this.namesOfTeamMembers = namesOfTeamMembers;
+    }
+}
+
+class Menu {
+
+    private char option;
+    Scanner scan = new Scanner(System.in);
+
+    public void displayMenu() {
+        System.out.println("\n\n\tAbout (A)");
         System.out.println("\tCreate Project (C)");
         System.out.println("\tEnter Votes (V)");
         System.out.println("\tShow Project (S)");
         System.out.println("\tQuit (Q)");
         System.out.print("\n\tPlease choose an option: ");
-        char input = scan.next().charAt(0);
-        choice = Character.toString(input);
 
-        switch (choice)
-        {
-            case valueOf('A'): MainMenu.About();
-                break;
-            case valueOf('C'): MainMenu.CreateProject();
-                break;
-            case valueOf('V'): MainMenu();
-                break;
-            case valueOf('S'): MainMenu();
-                break;
-            case valueOf('Q'): MainMenu.Quit();
-                break;
+        choose();
+    }
+
+    public void about() {
+        System.out.println("\n\tThis is a program designed to allocate credit fairly for a project based on each team member's contribution.");
+        System.out.print("\tPress any key to return to the Main Menu: ");
+        if (scan.next() != null) {
+            displayMenu();
         }
     }
 
-    //------------------------------------------------------------------------
-// Directs to the 'About' page for when user chooses option 'A'.
-//------------------------------------------------------------------------
-
-    private void About()
-    {
-        System.out.println("This program is a Fair Grade Allocator, with the purpose ");
-        System.out.println("of helping teams allocate the credit for a project fairly ");
-        System.out.println("so that all parties are satisfied with the outcome.");
+    public void newProject() {
+        Project p = new Project(); //think of a more intuitive name for "p".
+        String[] names = new String[10];
+        System.out.print("\tEnter the project name: ");
+        String name = scan.next();
+        p.setName(name);
+        System.out.print("\tEnter the number of team members: ");
+        String NumberTeamMembers = scan.next(); //change this to scan for int.
+        int n = Integer.parseInt(NumberTeamMembers);
+        for (int i = 1; i <= n; i++) {
+            System.out.print("\tEnter the name of team member " + i + ": ");
+            names[i] = scan.next();
+        }
+        p.setNamesOfTeamMembers(names);
+        System.out.print("\tPress any key to return to the Main Menu: ");
+        if (scan.next() != null) {
+            displayMenu();
+        }
     }
 
-//------------------------------------------------------------------------
-// Returns to Main Menu page for when user chooses options 'V' or 'S'.
-//------------------------------------------------------------------------
+    public int choose() {
+        char userInput = scan.next().charAt(0);
+        option = Character.toLowerCase(userInput);
+        switch (option) { //should these be upper case?
+            case 'a':
+                about();
+            case 'c':
+                newProject();
+            case 'v':
+                displayMenu();
+            case 's':
+                displayMenu();
+            case 'q':
+                System.exit(0);
+            default: {
+                System.out.println("Please choose another option: ");
+            }
+        }
 
-
-//------------------------------------------------------------------------
-// End of application for when user chooses option 'Q'.
-//------------------------------------------------------------------------
-
-    private void Quit()
-    {
-        System.out.print("END OF PROGRAMME");
+        return 0;
     }
 
-//------------------------------------------------------------------------
-// New Project page for when user chooses option 'C'.
-//------------------------------------------------------------------------
-    private void CreateProject()
-    {
-        System.out.println();
-    }
 }
