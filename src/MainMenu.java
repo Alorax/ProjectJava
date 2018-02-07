b5931dcba94bcaf3f497f8b9e6d99fe8f6f778c7
Miruna Serian <zcbtser@ucl.ac.uk>
2018-02-07 13:22:05 +0000

    Update MainMenu.java

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -13,72 +13,97 @@
 //------------------------------------------------------------------------
 
 import java.util.Scanner;
-import static java.lang.String.*;
 
-public class MainMenu
-{
-    //Creating the Main Menu options
 
-    public static void main(String[] args)
-    {
-        Scanner scan = new Scanner(System.in);
+class Project {
+    String name;
+    String[] namesOfTeamMembers;
 
-        //Main Menu content
-        System.out.println("Welcome to Split-it");
-        System.out.println("\n\tAbout (A)");
-        System.out.println("\tCreate Project (C)");
-        System.out.println("\tEnter Votes (V)");
-        System.out.println("\tShow Project (S)");
-        System.out.println("\tQuit (Q)");
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getNamesOfTeamMembers() {
+        return namesOfTeamMembers;
+    }
+
+    public void setNamesOfTeamMembers(String[] namesOfTeamMembers) {
+        this.namesOfTeamMembers = namesOfTeamMembers;
+    }
+}
+class Menu {
+
+    private char option;
+    Scanner scan = new Scanner(System.in);
+
+    public void displayMenu() {
+        System.out.println("\n\n\tAbout(A)");
+        System.out.println("\tCreate Project(C)");
+        System.out.println("\tEnter Votes(V( ")
+        System.out.println("\tShow Project(S)");
+        System.out.println("\tQuit(Q)");
         System.out.print("\n\tPlease choose an option: ");
-        char input = scan.next().charAt(0);
-        choice = Character.toString(input);
+        c
+    }
 
-        switch (choice)
-        {
-            case valueOf('A'): MainMenu.About();
-                break;
-            case valueOf('C'): MainMenu.CreateProject();
-                break;
-            case valueOf('V'): MainMenu();
-                break;
-            case valueOf('S'): MainMenu();
-                break;
-            case valueOf('Q'): MainMenu.Quit();
-                break;
+    public void about() {
+        System.out.print("\tThis is a program designed to assign grades for a project based on each member's participation developed by Anima and Miruna. ");
+        displayMenu();
+    }
+
+    public void newProject() {
+
+        Project p = new Project();
+        String[] names = new String[10];
+        System.out.print(" Enter the project name: ");
+        String name = scan.next();
+        p.setName(name);
+        System.out.print(" Enter the number of team members: ");
+        String NumberTeamMembers = scan.next();
+        int n = Integer.parseInt(NumberTeamMembers);
+        for (int i = 1; i <= n; i++) {
+            System.out.print("\t Enter the name of team member " + i +": ");
+            names[i] = scan.next();
+        }
+        p.setNamesOfTeamMembers(names);
+        System.out.print("Press any key to return to the main menu: ");
+        if (scan.next() != null) {
+            displayMenu();
         }
     }
 
-    //------------------------------------------------------------------------
-// Directs to the 'About' page for when user chooses option 'A'.
-//------------------------------------------------------------------------
+    public int choose() {
+        char userInput = scan.next().charAt(0);
+        option = Character.toLowerCase(userInput);
+        switch (option) {
+            case 'a':
+                about();
+            case 'c':
+                newProject();
+            case 'v':
+                displayMenu();
+            case 's':
+                displayMenu();
+            case 'q':
+                System.exit(0);
+            default: {
+                System.out.println("Please choose another option");
+                choose();
+            }
+        }
 
-    private void About()
-    {
-        System.out.println("This program is a Fair Grade Allocator, with the purpose ");
-        System.out.println("of helping teams allocate the credit for a project fairly ");
-        System.out.println("so that all parties are satisfied with the outcome.");
+        return 0;
     }
 
-//------------------------------------------------------------------------
-// Returns to Main Menu page for when user chooses options 'V' or 'S'.
-//------------------------------------------------------------------------
+}
+public class Main{
+    public static void main(String[] args) {
+       Menu m = new Menu();
+       m.displayMenu();
 
-
-//------------------------------------------------------------------------
-// End of application for when user chooses option 'Q'.
-//------------------------------------------------------------------------
-
-    private void Quit()
-    {
-        System.out.print("END OF PROGRAMME");
-    }
-
-//------------------------------------------------------------------------
-// New Project page for when user chooses option 'C'.
-//------------------------------------------------------------------------
-    private void CreateProject()
-    {
-        System.out.println();
     }
 }
