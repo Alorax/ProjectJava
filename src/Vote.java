fd39c83842f50d67d34af28ac2fe920638b80cae
Miruna Serian <zcbtser@ucl.ac.uk>
2018-03-05 23:38:46 +0000

    Update Vote.java

--- src/Vote.java
+++ src/Vote.java
@@ -4,36 +4,43 @@
 
 import java.util.ArrayList;
 //-----------------------------------------------------------------------------------
-//    Store the votes of each member
+//    Stores the votes of each member
 //-----------------------------------------------------------------------------------
-public class Vote {
+public class Vote
+{
 
    private String member;
 
    private ArrayList<Member> listOfMembersAndVotes ;
 
-    public Vote(String member,ArrayList<Member> listOfMembersAndVotes) {
+    public Vote(String member,ArrayList<Member> listOfMembersAndVotes) 
+    {
         this.member =member;
         this.listOfMembersAndVotes = listOfMembersAndVotes;
     }
 
-    public String getMember() {
+    public String getMember()
+    {
         return member;
     }
 
-    public void setMember(String member) {
+    public void setMember(String member) 
+    {
         this.member = member;
     }
 
-    public ArrayList<Member> getListOfMembersAndVotes() {
+    public ArrayList<Member> getListOfMembersAndVotes() 
+    {
         return listOfMembersAndVotes;
     }
 
-    public void setListOfMembersAndVotes(ArrayList<Member> listOfMembersAndVotes) {
+    public void setListOfMembersAndVotes(ArrayList<Member> listOfMembersAndVotes) 
+    {
         this.listOfMembersAndVotes = listOfMembersAndVotes;
     }
 
-    public String  printListOfMemberAndVotes(){
+    public String  printListOfMemberAndVotes()
+    {
         String list = null;
 
         if(listOfMembersAndVotes != null)
@@ -46,7 +53,8 @@ public class Vote {
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return  member + " , " + printListOfMemberAndVotes();
     }
 }
