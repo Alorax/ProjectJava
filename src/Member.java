ca340f00d14da0b57e0b782e4eb557ad7aae4213
Miruna Serian <zcbtser@ucl.ac.uk>
2018-04-03 13:22:01 +0100

    final version without comments

--- src/Member.java
+++ src/Member.java
@@ -1,54 +1,64 @@
 //----------------------------------------------------------------
 // Stores the information for each member
 //----------------------------------------------------------------
-public class Member {
+public class Member 
+{
 
     private String name;
     private int vote;
     private double ratio;
     private double finalShare;
 
-    public Member(String name, int vote) {
+    public Member(String name, int vote) 
+    {
         this.name = name;
         this.vote = vote;
         this.ratio = 1;
-
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name) 
+    {
         this.name = name;
     }
 
-    public int getVote() {
+    public int getVote()
+    {
         return vote;
     }
 
-    public void setVote(int vote) {
+    public void setVote(int vote)
+    {
         this.vote = vote;
     }
 
-    public double getRatio() {
+    public double getRatio() 
+    {
         return ratio;
     }
 
-    public void setRatio(double ratio) {
+    public void setRatio(double ratio) 
+    {
         this.ratio = ratio;
     }
 
-    public double getFinalShare() {
+    public double getFinalShare() 
+    {
         return finalShare;
     }
 
-    public void setFinalShare(double finalShare) {
+    public void setFinalShare(double finalShare) 
+    {
         this.finalShare = finalShare;
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return name + " , " + vote;
     }
 }
